247da598893bc1d9087b014864ab31b742610afc
Andrew Hoang <ahoang18@bu.edu>
2017-03-19 16:43:35 -0400

    Restructure directory and fix linting errors in index file

--- index.ios.js
+++ index.ios.js
@@ -1,185 +1,153 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-/*
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   AppRegistry,
-  StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  Navigator,
+  TouchableOpacity
+} from 'react-native'
 
-class dirty extends Component {
-  render() {
+var LoginPage = require('./src/pages/login')
+var RegistrationPage = require('./src/pages/registration')
+var PassRecoveryPage = require('./src/pages/passwordRecovery')
+var ResetPasswordPage = require('./src/pages/resetPassword')
+var SignUpPage = require('./src/pages/signUp')
+var AccountPage = require('./src/pages/account')
+var OrderPage = require('./src/pages/order')
+var PricingPage = require('./src/pages/pricing')
+var FAQPage = require('./src/pages/faq')
+
+class dirtydrawz extends Component {
+
+  constructor (props) {
+    super(props)
+    this._setNavigatorRef = this._setNavigatorRef.bind(this)
+    this.state = {
+      first_name: '',
+      last_name: '',
+      username: '',
+      passwd: '',
+      address: '',
+      email: '',
+      phone: '',
+      zipcode: ''
+    }
+  }
+
+  render () {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <Navigator
+        initialRoute={{id: 'login', name: 'login'}}
+        renderScene={this.renderScene.bind(this)}
+      />)
+  }
+
+  renderScene (route, navigator) {
+    if (route.id === 'login') {
+      return (
+        <LoginPage
+          msger_username={(para) => this.setState({username: para})}
+          msger_passwd={(para) => this.setState({passwd: para})}
+          test={this.state.first_name}
+          navigator={navigator} />
+      )
+    } else if (route.id === 'register') {
+      return (
+        <RegistrationPage
+          msger_first_name={(para) => this.setState({first_name: para})}
+          msger_last_name={(para) => this.setState({last_name: para})}
+          msger_address={(para) => this.setState({address: para})}
+          msger_email={(para) => this.setState({email: para})}
+          msger_phone={(para) => this.setState({phone: para})}
+          msger_zipcode={(para) => this.setState({zipcode: para})}
+          navigator={navigator} />
+      )
+    } else if (route.id === 'passrecover') {
+      return (
+        <PassRecoveryPage
+          navigator={navigator} />
+      )
+    } else if (route.id === 'resetpass') {
+      return (
+        <ResetPasswordPage
+          navigator={navigator} />
+      )
+    } else if (route.id === 'signup') {
+      return (
+        <SignUpPage
+          navigator={navigator} />
+      )
+    } else if (route.id === 'account') {
+      return (
+        <AccountPage
+          msger_first_name={(para) => this.setState({first_name: para})}
+          msger_last_name={(para) => this.setState({last_name: para})}
+          msger_address={(para) => this.setState({address: para})}
+          msger_email={(para) => this.setState({email: para})}
+          msger_phone={(para) => this.setState({phone: para})}
+          msger_zipcode={(para) => this.setState({zipcode: para})}
+          first_name={this.state.first_name}
+          last_name={this.state.last_name}
+          email={this.state.email}
+          phone={this.state.phone}
+          address={this.state.address}
+          zipcode={this.state.zipcode}
+          navigator={navigator} />
+      )
+    } else if (route.id === 'order') {
+      return (
+        <OrderPage
+          navigator={navigator} />
+      )
+    } else if (route.id === 'pricing') {
+      return (
+        <PricingPage
+          navigator={navigator} />
+      )
+    } else if (route.id === 'faq') {
+      return (
+        <FAQPage
+          navigator={navigator} />
+      )
+    }
+    return this.noRoute(navigator)
+  }
+
+  noRoute (navigator) {
+    return (
+      <View style={{flex: 1, alighItems: 'stretch', justifyContent: 'center'}}>
+        <TouchableOpacity
+          style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}
+          onPress={() => navigator.pop()}>
+          <Text style={{color: 'red', fontWeight: 'bold'}}> Hello please index.js at renderScene I am confused </Text>
+        </TouchableOpacity>
       </View>
-    );
+    )
+  }
+
+  _setNavigatorRef (navigator) {
+    if (navigator !== this._navigator) {
+      this._navigator = navigator
+
+      if (navigator) {
+        var callback = (event) => {
+          console.log(
+            `NavigatorMenu: event ${event.type}`,
+            {
+              route: JSON.stringify(event.data.route),
+              target: event.target,
+              type: event.type
+            }
+          )
+        }
+        // Observe focus change events from the owner.
+        this._listeners = [
+          navigator.navigationContext.addListener('willfocus', callback),
+          navigator.navigationContext.addListener('didfocus', callback)
+        ]
+      }
+    }
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('dirty', () => dirty);*/
-import React, { Component } from 'react';
-import ReactNative from 'react-native';
-import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View,
-    Navigator,
-    TouchableOpacity,
-} from 'react-native';
-
-var LoginPage = require('./android/pages/login')
-var RegistrationPage = require('./android/pages/registration')
-var PassRecoveryPage = require('./android/pages/passwordRecovery')
-var ResetPasswordPage = require('./android/pages/resetPassword')
-var SignUpPage = require('./android/pages/signUp')
-var AccountPage = require('./android/pages/account')
-var OrderPage = require('./android/pages/order')
-var PricingPage = require('./android/pages/pricing')
-var FAQPage = require('./android/pages/faq')
-
-class dirtydrawz extends Component {
-    
-    constructor(props) {
-        super(props);
-        this._setNavigatorRef = this._setNavigatorRef.bind(this);
-    }
-    render() {
-        return (
-                <Navigator
-                initialRoute ={{id:'login', name:'login'}}
-                renderScene={this.renderScene.bind(this)}
-                />
-                );
-    }
-    
-    renderScene(route, navigator) {
-        var routeID = route.id;
-        if(routeID === 'login') {
-            return (
-                    <LoginPage
-                    navigator={navigator} />
-                    );
-        }
-        if(routeID === 'register') {
-            return (
-                    <RegistrationPage
-                    navigator={navigator} />
-                    );
-        }
-        if(routeID === 'passrecover') {
-            return (
-                    <PassRecoveryPage
-                    navigator={navigator} />
-                    );
-        }
-        if(routeID === 'resetpass') {
-            return (
-                    <ResetPasswordPage
-                    navigator={navigator} />
-                    );
-        }
-        if(routeID === 'signup') {
-            return (
-                    <SignUpPage
-                    navigator={navigator} />
-                    );
-        }
-        if(routeID === 'account') {
-            return (
-                    <AccountPage
-                    navigator={navigator} />
-                    );
-        }
-        if(routeID === 'order')	{
-            return (
-                    <OrderPage
-                    navigator={navigator} />
-                    );
-        }
-        if(routeID === 'pricing')	{
-            return(
-                   <PricingPage
-                   navigator={navigator} />
-                   );
-        }
-        if(routeID === 'faq')	{
-            return(
-                   <FAQPage
-                   navigator={navigator} />
-                   );
-        }
-        return this.noRoute(navigator);
-    }
-    noRoute(navigator) {
-        return (
-                <View styel={{flex:1, alighItems: 'stretch', justifyContent: 'center'}}>
-                <TouchableOpacity style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}
-                onPress={() => navigator.pop()}>
-                <Text style={{color: 'red', fontWeight: 'bold'}}> Hello please index.js at renderScene I am confused </Text>
-                </TouchableOpacity>
-                </View>
-                );
-    }
-    
-    _setNavigatorRef(navigator) {
-        if (navigator !== this._navigator) {
-            this._navigator = navigator;
-            
-            if (navigator) {
-                var callback = (event) => {
-                    console.log(
-                                `NavigatorMenu: event ${event.type}`,
-                                {
-                                route: JSON.stringify(event.data.route),
-                                target: event.target,
-                                type: event.type,
-                                }
-                                );
-                };
-                // Observe focus change events from the owner.
-                this._listeners = [
-                                   navigator.navigationContext.addListener('willfocus', callback),
-                                   navigator.navigationContext.addListener('didfocus', callback),
-                                   ];
-            }
-        }
-    }
-}
-
-
-AppRegistry.registerComponent('dirtydrawz', () => dirtydrawz);
+AppRegistry.registerComponent('dirtydrawz', () => dirtydrawz)
